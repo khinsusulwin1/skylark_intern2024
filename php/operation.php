feda9a16199c5fb4bc8f6ebc34cf3098acd495d4
khinsusulwin1 <169069592+khinsusulwin1@users.noreply.github.com>
2024-05-07 14:16:23 +0630

    Add files via upload

---
+++ php/operation.php
@@ -0,0 +1,12 @@
+<?php
+  $a=42;
+  $b=20;
+  $c=$a+$b;
+  echo "Addition Operation Result:$c\n<br>";
+  $c=$a-$b;
+  echo "Substration Operation Result:$c\n<br>";
+  $c=$a*$b;
+  echo "Multiplication Operation Result:$c\n<br>";
+  $c=$a%$b;
+  echo "Division Operation Result:$c\n";
+?>
